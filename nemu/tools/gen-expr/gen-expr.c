0273b5c5cff59521468532664f7298f0cc99295c
tracer-ics2019 <tracer@njuics.org>
2019-09-18 19:19:35 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     19:19:35 up  2:42,  1 user,  load average: 0.21, 0.25, 0.26
    77434b6fea432af380cc95b3b06440b657abc7db

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -5,10 +5,63 @@
 #include <assert.h>
 #include <string.h>
 
+uint32_t choose(uint32_t n);
+
+void gen_num();
+void gen();
+void gen_rand_op();
+void gen(char str);
+
+int index = 0;
+
 // this should be enough
 static char buf[65536];
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
+}
+
+
+uint32_t choose(uint32_t n);{
+  uint32_t rand_int = rand() % (n+1);
+};
+
+void gen_num(){
+
+  int randint = rand();
+  int t=0,mask=1;
+	t=randint;
+	while(t>9){
+		t = t/10;
+		mask = mask*10;
+	}
+	while(mask>0){
+	 	buf[index]=((randint/mask)+'0');
+    index++; 
+		randint = randint % mask;
+		mask = mask/10;
+	} 
+}
+
+void gen(char str){
+  buf[index] = str;
+  index++;
+}
+
+void gen_rand_op(){
+  int choice = choose (3);
+  char op;
+  switch(choice) {
+    case 0: op = '+'; break;
+    case 1: op = '-'; break;
+    case 2: op = '/'; break;
+    case 3: op = '*'; break; 
+  }
+  buf[index] = op;
+  index++;
 }
 
 static char code_buf[65536];
@@ -45,6 +98,7 @@ int main(int argc, char *argv[]) {
     assert(fp != NULL);
 
     int result;
+    /*fscanf可以从一个文件流中格式化读出数据，遇到空格或回车就停止*/
     fscanf(fp, "%d", &result);
     pclose(fp);
 
