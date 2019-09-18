d3ca97088ed683f6ed3b40869879574dc277bdf9
tracer-ics2019 <tracer@njuics.org>
2019-09-18 22:37:15 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     22:37:15 up  5:15,  1 user,  load average: 0.64, 0.44, 0.33
    b42a1376293ff301a7dbbb2371ae7135d07f0d52

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -28,7 +28,7 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  {" +", TK_NOTYPE},        // spaces
+  {"\\s+", TK_NOTYPE},        // spaces
   {"\\+", '+'},             // plus
   {"==", TK_EQ},            // equal
   {"\\-", '-'},             // minus
