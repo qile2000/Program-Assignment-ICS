8a66e1a07cae15cdc68baf5309bfd5a9ba4310ca
tracer-ics2019 <tracer@njuics.org>
2019-09-20 20:51:35 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     20:51:35 up  3:44,  1 user,  load average: 0.21, 0.12, 0.09
    52e307f2a1b98d197295aba0ecac1146ab1b97a2

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   
   int is_batch_mode = init_monitor(argc, argv);
-  char the_expr[256];
+  char the_expr[1000];
   FILE *fp=fopen("../nemu/tools/gen-expr/input","r");
   if(!fp){
     printf("can't open file\n");
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
   }
   while(!feof(fp))
   {
-    fgets(the_expr,256, fp);
+    fgets(the_expr,1000, fp);
     char *answer = strtok(the_expr, " ");
     char *expression = strtok(NULL, " ");
     bool suc=true;
