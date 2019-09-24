32b10defdcdcef3e43ab865f4233e8f889cec517
tracer-ics2019 <tracer@njuics.org>
2019-09-24 20:50:22 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     20:50:22 up  3:57,  1 user,  load average: 0.06, 0.19, 0.17
    aada788ef8c83f8deb520292793b4d6b7342d22c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -122,7 +122,7 @@ static bool make_token(char *e) {
           }break;
           case TK_TEN_NUM:case TK_REG:case TK_SIXTEEN_NUM:{
             tokens[nr_token].type=rules[i].token_type;
-            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            strcpy(tokens[nr_token].str, substr_start);
             nr_token++;
           }break;
           default: {
