0a718d8a0c130a8b5f5c70a0243f0979f2ae40a8
tracer-ics2019 <tracer@njuics.org>
2019-09-24 21:31:30 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     21:31:30 up  4:38,  1 user,  load average: 0.14, 0.12, 0.14
    a4fbe5571a43ff3a2651430038ab64a05d09ff6b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -234,7 +234,7 @@ uint32_t eval(int p, int q){
       int reg_value = 0;
       char reg_name_32[4];
       reg_name_32[0] = '$';
-      printf("%s",strcat(reg_name_32,regsl_copy[0]));
+      printf("666%s666\n",strcat(reg_name_32,regsl_copy[0]));
       for (int i=0; i<=7; i++){
         if (strcmp(strcat(reg_name_32,regsl_copy[i]),tokens[p].str)==0){
           reg_value = cpu.gpr[i]._32;
