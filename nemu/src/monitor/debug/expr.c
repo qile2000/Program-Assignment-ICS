b03b04f9dd043dc9b84b6ee5f3503103183edb45
tracer-ics2019 <tracer@njuics.org>
2019-09-24 21:57:18 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     21:57:18 up  5:04,  1 user,  load average: 0.17, 0.14, 0.16
    7f9fbe2f8a5cc69cf00f652e693ce394d0658c7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -232,24 +232,23 @@ uint32_t eval(int p, int q){
     }
     else if (tokens[p].type == TK_REG){
       if (strlen(tokens[p].str)==4){
-        char reg_name[3];
-        reg_name[0]=tokens[p].str[1];
-        reg_name[1]=tokens[p].str[2];
-        reg_name[2]=tokens[p].str[3];
+        char reg_name_32[3];
+        reg_name_32[0]=tokens[p].str[1];
+        reg_name_32[1]=tokens[p].str[2];
+        reg_name_32[2]=tokens[p].str[3];
         for (int i=0; i<=7; i++){
-          if (strcmp(reg_name,regsl_copy[i])==0){
+          if (strcmp(reg_name_32,regsl_copy[i])==0){
             return cpu.gpr[i]._32;
           }
         }
       }
       
       else if (strlen(tokens[p].str)==3){
-        char reg_name[2];
-        reg_name[0]=tokens[p].str[1];
-        reg_name[1]=tokens[p].str[2];
-        printf("%s",reg_name);
+        char reg_name_16[2];
+        reg_name_16[0]=tokens[p].str[1];
+        reg_name_16[1]=tokens[p].str[2];
         for (int i=0; i<=7; i++){
-          if (strcmp(reg_name,regsw_copy[i])==0){
+          if (strcmp(reg_name_16,regsw_copy[i])==0){
             return cpu.gpr[i]._16;
           }
         }
