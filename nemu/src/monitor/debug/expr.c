a9c388b32a3604664dc83d9a9dd0415fb88db95f
tracer-ics2019 <tracer@njuics.org>
2019-09-24 21:18:07 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     21:18:07 up  4:25,  1 user,  load average: 0.75, 0.45, 0.25
    ffa9a6987cf40ec45aa928a41cb0ed5393118a43

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -232,11 +232,10 @@ uint32_t eval(int p, int q){
     }
     else if (tokens[p].type == TK_REG){
       int reg_value = 0;
-      char reg_name[50];
+      char reg_name[20];
       reg_name[0] = '$';
       for (int i=0; i<=7; i++){
         if (strcmp(strcat(reg_name,regsl_copy[i]),tokens[p].str)==0){
-          
           reg_value = cpu.gpr[i]._32;
           break;
         }
