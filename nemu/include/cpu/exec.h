06cf034ad7d27067ac71863421ab6b542e490a0a
tracer-ics2019 <tracer@njuics.org>
2019-10-20 00:29:27 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     00:29:27 up 1 day,  1:19,  1 user,  load average: 0.00, 0.03, 0.05
    f1dd8d4ee47b351d9ccd8bf1a3a166c3edb20265

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -24,6 +24,7 @@ typedef struct {
 #define EMPTY              EX(inv)
 
 static inline uint32_t instr_fetch(vaddr_t *pc, int len) {
+  //从pc位置读内存
   uint32_t instr = vaddr_read(*pc, len);
 #ifdef DEBUG
   uint8_t *p_instr = (void *)&instr;
