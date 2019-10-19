29d3c3865defe7279147606260df3efce93e5984
tracer-ics2019 <tracer@njuics.org>
2019-10-19 15:54:32 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     15:54:32 up 16:44,  1 user,  load average: 0.00, 0.03, 0.00
    59d894b78993eed2914bf7d8cfbcfa15aa178ebd

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -1,9 +1,14 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov);
-make_EHelper(call);
-make_EHelper(push);
 make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+make_EHelper(call);			// control.c
+make_EHelper(push); 		//data-mov.c
+make_EHelper(sub); 			//arith.c
+make_EHelper(xor); 			//logic.c
+make_EHelper(pop);			//data-mov.c
+make_EHelper(ret);  		//control.c
