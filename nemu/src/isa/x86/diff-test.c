6e9419c08c07b6350c9f8c9ad91f5fa80bcc5fba
tracer-ics2019 <tracer@njuics.org>
2019-10-25 17:17:55 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     17:17:55 up  6:32,  1 user,  load average: 1.09, 0.81, 0.47
    6da6c40ab1ef665779a66e59a5782a273ef068de

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,18 +2,15 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if ( ref_r->eax != cpu.eax
-    || ref_r->ecx != cpu.ecx
-    || ref_r->edx != cpu.edx
-    || ref_r->ebx != cpu.ebx
-    || ref_r->esp != cpu.esp
-    || ref_r->ebp != cpu.ebp
-    || ref_r->esi != cpu.esi
-    || ref_r->edi != cpu.edi
-    || pc != cpu.pc)
-  {
-    return false;
-  }
+  if ( ref_r->eax != cpu.eax){printf("ref.eax: %d, cpu.eax: %d\n",ref_r->eax,cpu.eax); return false;}
+  if ( ref_r->ecx != cpu.ecx){printf("ref.ecx: %d, cpu.ecx: %d\n",ref_r->ecx,cpu.ecx); return false;}
+  if ( ref_r->edx != cpu.edx){printf("ref.edx: %d, cpu.edx: %d\n",ref_r->edx,cpu.edx); return false;}
+  if ( ref_r->ebx != cpu.ebx){printf("ref.ebx: %d, cpu.ebx: %d\n",ref_r->ebx,cpu.ebx); return false;}
+  if ( ref_r->esp != cpu.esp){printf("ref.esp: %d, cpu.esp: %d\n",ref_r->esp,cpu.esp); return false;}
+  if ( ref_r->ebp != cpu.ebp){printf("ref.ebp: %d, cpu.ebp: %d\n",ref_r->ebp,cpu.ebp); return false;}
+  if ( ref_r->esi != cpu.esi){printf("ref.esi: %d, cpu.esi: %d\n",ref_r->esi,cpu.esi); return false;}
+  if ( ref_r->edi != cpu.edi){printf("ref.edi: %d, cpu.edi: %d\n",ref_r->edi,cpu.edi); return false;}
+  if ( pc != cpu.pc){printf("ref's pc: %x, cpu.pc: %x\n",pc,cpu.pc); return false;}
   return true;
 }
 
