0fb3ad7247d91fa4de468c90df33a8cd9e61f5d2
181220075-Zhou Qile <qile0402@163.com>
2019-10-25 20:57:41 +0800

    rtl_sext v1.0 and dophelper SI

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,7 +137,9 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  int32_t sext = (int32_t)* src1;
+  sext = sext << 32 - (8 * width);
+  *dest = sext >> 32 - (8 * width);
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
