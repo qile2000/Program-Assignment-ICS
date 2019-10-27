53ac1edf1ab0cbb344f8080cc08fc9c354e17372
tracer-ics2019 <tracer@njuics.org>
2019-10-27 17:19:30 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     17:19:30 up  3:35,  1 user,  load average: 0.60, 0.29, 0.17
    cafa7f12e9d4fd0f6a7e754ab0aacfe2e451c9

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,16 +2,16 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if ( ref_r->eax != cpu.eax){printf("ref.eax: %d, \ncpu.eax: %d\n",ref_r->eax,cpu.eax); return false;}
-  if ( ref_r->ecx != cpu.ecx){printf("ref.ecx: %d, \ncpu.ecx: %d\n",ref_r->ecx,cpu.ecx); return false;}
-  if ( ref_r->edx != cpu.edx){printf("ref.edx: %d, \ncpu.edx: %d\n",ref_r->edx,cpu.edx); return false;}
-  if ( ref_r->ebx != cpu.ebx){printf("ref.ebx: %d, \ncpu.ebx: %d\n",ref_r->ebx,cpu.ebx); return false;}
-  if ( ref_r->esp != cpu.esp){printf("ref.esp: %d, \ncpu.esp: %d\n",ref_r->esp,cpu.esp); return false;}
-  if ( ref_r->ebp != cpu.ebp){printf("ref.ebp: %d, \ncpu.ebp: %d\n",ref_r->ebp,cpu.ebp); return false;}
-  if ( ref_r->esi != cpu.esi){printf("ref.esi: %d, \ncpu.esi: %d\n",ref_r->esi,cpu.esi); return false;}
-  if ( ref_r->edi != cpu.edi){printf("ref.edi: %d, \ncpu.edi: %d\n",ref_r->edi,cpu.edi); return false;}
+  if ( ref_r->eax != cpu.eax){printf("pc at %x: ref.eax: %d, \ncpu.eax: %d\n",cpu.pc,ref_r->eax,cpu.eax); return false;}
+  if ( ref_r->ecx != cpu.ecx){printf("pc at %x: ref.ecx: %d, \ncpu.ecx: %d\n",cpu.pc,ref_r->ecx,cpu.ecx); return false;}
+  if ( ref_r->edx != cpu.edx){printf("pc at %x: ref.edx: %d, \ncpu.edx: %d\n",cpu.pc,ref_r->edx,cpu.edx); return false;}
+  if ( ref_r->ebx != cpu.ebx){printf("pc at %x: ref.ebx: %d, \ncpu.ebx: %d\n",cpu.pc,ref_r->ebx,cpu.ebx); return false;}
+  if ( ref_r->esp != cpu.esp){printf("pc at %x: ref.esp: %d, \ncpu.esp: %d\n",cpu.pc,ref_r->esp,cpu.esp); return false;}
+  if ( ref_r->ebp != cpu.ebp){printf("pc at %x: ref.ebp: %d, \ncpu.ebp: %d\n",cpu.pc,ref_r->ebp,cpu.ebp); return false;}
+  if ( ref_r->esi != cpu.esi){printf("pc at %x: ref.esi: %d, \ncpu.esi: %d\n",cpu.pc,ref_r->esi,cpu.esi); return false;}
+  if ( ref_r->edi != cpu.edi){printf("pc at %x: ref.edi: %d, \ncpu.edi: %d\n",cpu.pc,ref_r->edi,cpu.edi); return false;}
   //if ( pc != cpu.pc){printf("ref's pc: %x, \ncpu.pc: %x\n",pc,cpu.pc); return false;}
-  printf("temporary, regs is the same\n");
+  printf("pc at %x: temporary, regs is the same\n",cpu.pc);
   return true;
 }
 
