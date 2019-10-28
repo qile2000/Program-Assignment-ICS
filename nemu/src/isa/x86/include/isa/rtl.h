66cb052b2089798219372ebf9f6b535e0510f38a
tracer-ics2019 <tracer@njuics.org>
2019-10-28 21:24:20 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     21:24:20 up 14:51,  1 user,  load average: 0.24, 0.16, 0.09
    bc3bf62748624b1fefaa42041ae8f9ed6417c79

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -104,7 +104,7 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  cpu.eflags.SF = result[width*8 -1];
+  cpu.eflags.SF = *result>>(width*8 -1);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
