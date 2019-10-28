a8fabe6184061c577611d7dfba160b83fb9c83f5
tracer-ics2019 <tracer@njuics.org>
2019-10-28 20:53:48 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     20:53:48 up 14:20,  1 user,  load average: 0.07, 0.07, 0.11
    15d2d2b12f51c81882274873955c3dc4e6ea90f6

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,6 +137,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
+  /*
   if(width == 4)
 		*dest = *src1;
 	else if(width == 2)
@@ -150,11 +151,11 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 		int8_t tmp = result ;
 		*dest = tmp;
 	}
-  /*
+  */
   int32_t sext = (int32_t)* src1;
   sext = sext << (32 - (8 * width));
   *dest = sext >> (32 - (8 * width));
-  */
+  
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
