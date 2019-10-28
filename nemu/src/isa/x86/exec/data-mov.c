4ce61a96ae3ac2909b485d2abd36c2177bb951eb
tracer-ics2019 <tracer@njuics.org>
2019-10-28 20:02:56 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     20:02:56 up 13:29,  1 user,  load average: 0.43, 0.23, 0.13
    39ff6abb9181cb8a260f3ce9bde0ca7ad2fb11a1

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -7,8 +7,8 @@ make_EHelper(mov) {
 
 make_EHelper(push) {
   
-  s0 = id_dest->val;
-  rtl_push(&s0);
+  
+  rtl_push(&id_dest->val);
   //rtl_push(&s0);
   //if (id_dest->type == OP_TYPE_REG) { rtl_sr(id_dest->reg, &s0, id_dest->width); }//如果目的操作数是寄存器操作数,写入寄存器
   //else if (id_dest->type == OP_TYPE_MEM) { rtl_sm(&id_dest->addr, &s0, id_dest->width); }//如果是在内存里面，写入内存
