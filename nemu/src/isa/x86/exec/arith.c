6d275ac08fded2a420609b23d204af6026937bd4
tracer-ics2019 <tracer@njuics.org>
2019-10-28 19:30:53 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     19:30:53 up 12:57,  1 user,  load average: 0.95, 0.44, 0.21
    2b39ea041273f3258f8e8a6f4a23c7974fd4a211

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -3,30 +3,41 @@
 make_EHelper(add) {
   rtl_add(&s0,&(id_dest->val),&(id_src->val));
 	operand_write(id_dest,&s0);
+
 	rtl_update_ZFSF(&s0, id_dest->width);
+
 	rtl_is_add_overflow(&s1, &s0, &(id_dest->val), &(id_src->val), id_dest->width);
 	rtl_set_OF(&s1);
+
 	rtl_is_add_carry(&s1, &s0, &(id_dest->val));
-	rtl_set_CF(&s1);//
+	rtl_set_CF(&s1);
+
   print_asm_template2(add);
 }
 
 make_EHelper(sub) {
   rtl_sub(&s0,&(id_dest->val),&(id_src->val));
 	operand_write(id_dest,&s0);
+
 	rtl_update_ZFSF(&s0, id_dest->width);
+
 	rtl_is_sub_overflow(&s1, &s0, &(id_dest->val), &(id_src->val), id_dest->width);
 	rtl_set_OF(&s1);
+
 	rtl_is_sub_carry(&s1, &s0, &(id_dest->val));
-	rtl_set_CF(&s1);//
+	rtl_set_CF(&s1);
+
   print_asm_template2(sub);
 }
 
 make_EHelper(cmp) {
   rtl_sub(&s0, &(id_dest->val), &(id_src->val));
+
 	rtl_update_ZFSF(&s0, id_dest->width);
+
 	rtl_is_sub_overflow(&s1, &s0, &(id_dest->val), &(id_src->val), id_dest->width);
 	rtl_set_OF(&s1);
+
 	rtl_is_sub_carry(&s1, &s0, &(id_dest->val));
 	rtl_set_CF(&s1);
 
@@ -38,9 +49,12 @@ make_EHelper(inc) {
 
   rtl_add(&s0,&(id_dest->val),&t1);
 	operand_write(id_dest,&s0);
+
 	rtl_update_ZFSF(&s0, id_dest->width);
+
 	rtl_is_add_overflow(&s1, &s0, &(id_dest->val), &t1, id_dest->width);
 	rtl_set_OF(&s1);
+  
 	rtl_is_add_carry(&s1, &s0, &(id_dest->val));
 	rtl_set_CF(&s1);
 
