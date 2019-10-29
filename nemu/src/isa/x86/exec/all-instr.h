91164d7b54406872d5e996d74c2f17fb62dad011
181220075-Zhou Qile <qile0402@163.com>
2019-10-29 09:29:05 +0800

    test v1.0

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -32,3 +32,4 @@ make_EHelper(jmp);              //control.c
 make_EHelper(imul2);             //arith.c
 make_EHelper(movsx);            //data-mov.c
 make_EHelper(neg);              //arith.c
+make_EHelper(test);           //logic.c
