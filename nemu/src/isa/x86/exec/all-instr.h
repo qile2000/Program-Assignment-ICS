7d55452462e89c5c653a4ab2311e34c935b957f4
181220075-Zhou Qile <qile0402@163.com>
2019-10-29 09:44:08 +0800

    cltd v1.0

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -33,3 +33,4 @@ make_EHelper(imul2);             //arith.c
 make_EHelper(movsx);            //data-mov.c
 make_EHelper(neg);              //arith.c
 make_EHelper(test);           //logic.c
+make_EHelper(cltd);           //data-mov.c
--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -47,10 +47,20 @@ make_EHelper(leave) {
 
 make_EHelper(cltd) {
   if (decinfo.isa.is_operand_size_16) {
-    TODO();
+		if((int16_t)(reg_w(R_AX))<0){
+			reg_w(R_DX) = 0xffff;
+		}
+    else{
+			reg_w(R_DX) = 0;
+		}
   }
   else {
-    TODO();
+		if((int32_t)(reg_l(R_EAX))<0){
+			reg_l(R_EDX) = 0xffffffff;
+		}
+    else{
+			reg_l(R_EDX) = 0;
+		}
   }
 
   print_asm(decinfo.isa.is_operand_size_16 ? "cwtl" : "cltd");
