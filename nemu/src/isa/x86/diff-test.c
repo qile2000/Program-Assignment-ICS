20f61d94e894097c3e5411a97c8d73ed24bce571
tracer-ics2019 <tracer@njuics.org>
2019-10-29 23:22:35 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     23:22:35 up 1 day,  2:07,  1 user,  load average: 0.33, 0.34, 0.23
    43664ea5c693a6f37087d8d3ef11207d8ebc405e

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -4,7 +4,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if ( ref_r->eax != cpu.eax){printf("pc at %x: ref.eax: %d, cpu.eax: %d\n",cpu.pc,ref_r->eax,cpu.eax); return false;}
   if ( ref_r->ecx != cpu.ecx){printf("pc at %x: ref.ecx: %d, cpu.ecx: %d\n",cpu.pc,ref_r->ecx,cpu.ecx); return false;}
-  if ( ref_r->edx != cpu.edx){printf("pc at %x: ref.edx: %d, cpu.edx: %d\n",cpu.pc,ref_r->edx,cpu.edx); return false;}
+  if ( ref_r->edx != cpu.edx){ printf("ref.edx:%x  cpu.edx:%x  ref.ebx:%x  cpu.ebx:%x\n",ref_r->edx,cpu.edx,ref_r->ebx,cpu.ebx);}//printf("pc at %x: ref.edx: %d, cpu.edx: %d\n",cpu.pc,ref_r->edx,cpu.edx); return false;}
   if ( ref_r->ebx != cpu.ebx){printf("pc at %x: ref.ebx: %d, cpu.ebx: %d\n",cpu.pc,ref_r->ebx,cpu.ebx); return false;}
   if ( ref_r->esp != cpu.esp){printf("pc at %x: ref.esp: %d, cpu.esp: %d\n",cpu.pc,ref_r->esp,cpu.esp); return false;}
   if ( ref_r->ebp != cpu.ebp){printf("pc at %x: ref.ebp: %d, cpu.ebp: %d\n",cpu.pc,ref_r->ebp,cpu.ebp); return false;}
