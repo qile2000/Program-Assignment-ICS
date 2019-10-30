760a36f0d3312f7c3739de9c5fa893b18a593270
tracer-ics2019 <tracer@njuics.org>
2019-10-30 20:40:01 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     20:40:01 up  2:26,  1 user,  load average: 0.42, 0.56, 0.47
    91497e95e8fb5076ee22ac2c6858d0dbfce05fdd

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -107,7 +107,7 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  /*
+  
   assert((s1!=NULL)&&(s2!=NULL));
 	while(*s1!='\0'&&*s2!='\0'&&(n>0)){
 		if(*s1-*s2>0)
@@ -125,7 +125,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
     return 1;
   }
 	return 0; 
-  */
+  /*
   assert((s1 != NULL) && (s2 != NULL));
 	int a = 0;
 	while( (a = (*s1 - *s2)) == 0 && *s1 && *s2 && n--) {
@@ -133,6 +133,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 		s2++;
 	}
 	return a;
+*/
 }
 
 void* memset(void* v,int c,size_t n) {
