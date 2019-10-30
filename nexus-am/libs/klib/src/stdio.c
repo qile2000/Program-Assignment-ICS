171aef78dbd7d814c2c851594e4d885f7238a41d
tracer-ics2019 <tracer@njuics.org>
2019-10-31 01:33:45 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     01:33:45 up  6:13,  1 user,  load average: 0.03, 0.10, 0.09
    bad2dd3ac50e1fcc7befcce67b717b3557cafde0

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -14,15 +14,14 @@ va_list 是一个字符指针，可以理解为指向当前参数的一个指针
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 //arg 一个表示可变参数列表的对象。这应被 <stdarg> 中定义的 va_start 宏初始化
 
-
-static char * itoa(int num, char *str, int radix) {
+static char * itoa(int num, char *str, int base) {
   assert(num >= 0);
-  static char ascii[] = "0123456789abcdef";
   int count = 0;
-  do {
-    str[count++] = ascii[num % radix];
-    num /= radix;
-  } while (num != 0);
+  while (num != 0){
+    str[count] = (char)(num%base+'0');
+    num /= base;
+    count++;
+  }
   str[count] = '\0';
   for (int i = 0; i < count / 2; ++i) {
     char tmp = str[i];
@@ -31,6 +30,7 @@ static char * itoa(int num, char *str, int radix) {
   }
   return str;
 }
+
 int printf(const char *fmt, ...) {
   va_list args;
   char string[1000];
