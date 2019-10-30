dce55e44448f38348044b7b3fa8c7b53be471d1b
tracer-ics2019 <tracer@njuics.org>
2019-10-30 20:42:44 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     20:42:44 up  2:28,  1 user,  load average: 0.68, 0.56, 0.47
    c2a4ed9f9233840bb1581513e147dd3c5546584

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -79,7 +79,6 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  /*
   assert((s1!=NULL)&&(s2!=NULL));
 	while(*s1!='\0'&&*s2!='\0'){
 		if(*s1-*s2>0)
@@ -96,18 +95,17 @@ int strcmp(const char* s1, const char* s2) {
     return 1;
   }
 	return 0; 
-  */
+  /*
   int a = 0;
 	while( (a = (*s1 - *s2)) == 0 && *s1 && *s2) {
 		s1++;
 		s2++;
 	}
-	// printf("strcmp2 %d\n", a);
 	return a;
+  */
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  
   assert((s1!=NULL)&&(s2!=NULL));
 	while(*s1!='\0'&&*s2!='\0'&&(n>0)){
 		if(*s1-*s2>0)
@@ -124,44 +122,23 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 	if(*s2=='\0'&&*s1!='\0'){
     return 1;
   }
-	return 0; 
-  /*
-  assert((s1 != NULL) && (s2 != NULL));
-	int a = 0;
-	while( (a = (*s1 - *s2)) == 0 && *s1 && *s2 && n--) {
-		s1++;
-		s2++;
-	}
-	return a;
-*/
+	return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  /*
-  int *ptr = (int*)v;
-
-  while (n--)
-  {
-    *ptr++ = c;
+  void* ret = v;
+  while(n--){
+    *(char*)v = (char)c;
+    v = (char*)v + 1; //移动一个字节
   }
-
-  return v;
-  */
- void* ret = v;
-    while(n--)
-    {
-        *(char*)v = (char)c;
-        v = (char*)v + 1; //移动一个字节
-    }
-    return ret;
+  return ret;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
   const char *s = (char*)in;
   char *d = (char*)out;
 
-  while (n--)
-  {
+  while (n--){
     *d++ = *s++;
   }
 
@@ -171,10 +148,8 @@ void* memcpy(void* out, const void* in, size_t n) {
 int memcmp(const void* s1, const void* s2, size_t n){
   const unsigned char *str1 = (unsigned char *)s1;
   const unsigned char *str2 = (unsigned char *)s2;
-  while (n-- > 0)
-  {
-    if (*str1++ != *str2++)
-    {
+  while (n-- > 0){
+    if (*str1++ != *str2++){
       return str1[-1] < str2[-1] ? -1 : 1;
     }
   }
