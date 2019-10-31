b96d66e67e82c094286d4ef86ffa600c1049d2fa
181220075-Zhou Qile <qile0402@163.com>
2019-11-01 01:20:13 +0800

    keyboard v2.0

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -3,7 +3,6 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
-
 //keydown = 1为按下按键, keydown = 0为释放按键. 
 //keycode为按键的断码, 没有按键时, keycode为_KEY_NONE.
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
