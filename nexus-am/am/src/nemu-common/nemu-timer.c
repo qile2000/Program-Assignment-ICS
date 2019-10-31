3f143d5359c00d2ccc55aec54a19d3c99edce0c1
181220075-Zhou Qile <qile0402@163.com>
2019-10-31 19:23:23 +0800

    rtc.c v1.0

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -10,7 +10,6 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
       uptime->hi = 0;
       uptime->lo = inl(0x48)-init;
-      //printf("%d ",uptime->lo);
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
