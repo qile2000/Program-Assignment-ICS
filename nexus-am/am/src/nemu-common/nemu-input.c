3a43dffec4e6d4955ebaf2de8fe0f326ca9c245c
tracer-ics2019 <tracer@njuics.org>
2019-10-31 21:47:05 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     21:47:05 up  6:27,  1 user,  load average: 0.23, 0.24, 0.17
    a609e5fdbbf859b37d4b1d235362a5242a9fb5e

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -9,7 +9,7 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
       int fetch = inl(0x60);
-      kbd->keycode = inl(0x60);
+      kbd->keycode = fetch;
       if(fetch){
 	      kbd->keydown=0;
       }
