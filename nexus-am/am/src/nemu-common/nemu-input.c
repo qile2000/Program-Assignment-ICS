40f555db669662c4c98e69fd68ebd1303811dc11
tracer-ics2019 <tracer@njuics.org>
2019-11-01 01:15:28 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     01:15:28 up  9:44,  1 user,  load average: 0.13, 0.18, 0.10
    98a622c1538b6818fda7438624bdee06b5bc66

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -3,17 +3,16 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
-unsigned long long press;
+
 //keydown = 1为按下按键, keydown = 0为释放按键. 
 //keycode为按键的断码, 没有按键时, keycode为_KEY_NONE.
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      press = inl(0x60);
-      kbd->keycode = press;
-      if(press != _KEY_NONE){
-          kbd->keydown = !(kbd->keydown);
+      kbd->keycode = inl(0x60);
+      if(kbd->keycode){
+          kbd->keydown = 1 - kbd->keydown;
       }
       return sizeof(_DEV_INPUT_KBD_t);
     }
