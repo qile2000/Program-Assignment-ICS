832c7877dda5debf0a9a055cd683f14fe20231e6
tracer-ics2019 <tracer@njuics.org>
2019-11-02 01:21:33 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     01:21:33 up  2:11,  1 user,  load average: 0.26, 0.17, 0.20
    a0e3a596445331339111280d4317d22ef0ec2421

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -52,7 +52,40 @@ static inline void update_pc(void) {
 }
 
 void display_inv_msg(vaddr_t pc);
-
+make_EHelper(push);
+make_EHelper(call);
+make_EHelper(sub);
+make_EHelper(xor);
+make_EHelper(pop);
+make_EHelper(ret);
+make_EHelper(lea);
+make_EHelper(and);
+make_EHelper(nop);
+make_EHelper(add);
+make_EHelper(cmp);
+make_EHelper(setcc);
+make_EHelper(movzx);
+make_EHelper(test);
+make_EHelper(jcc);
+make_EHelper(leave);
+make_EHelper(adc);
+make_EHelper(or);
+make_EHelper(sar);
+make_EHelper(shl);
+make_EHelper(dec);
+make_EHelper(not);
+make_EHelper(inc);
+make_EHelper(imul2);
+make_EHelper(imul1);
+make_EHelper(cltd);
+make_EHelper(idiv);
+make_EHelper(jmp);
+make_EHelper(test);
+make_EHelper(movsx);
+make_EHelper(shr);
+make_EHelper(sbb);
+make_EHelper(out);
+make_EHelper(neg);
 #ifdef DEBUG
 #define print_asm(...) \
   do { \
