460a1d4f325ceb12b575b904a75d36115901d1e7
tracer-ics2019 <tracer@njuics.org>
2019-11-02 15:43:44 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     15:43:44 up  7:09,  1 user,  load average: 0.38, 0.33, 0.28
    7d6392de40b1d8186c30d3fd24d491c716f9d7fa

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -27,13 +27,13 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
       uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-      int pixel_count = 0;
       uint32_t *pixels = ctl->pixels;
+      int cp_bytes = min(w, W - x);
       for (int j = 0; j < h && y + j < H; j ++){
-        for (int i = 0; i < w && x + i < W; i ++){
-          fb[(j+y)*W+x+i] = pixels[pixel_count];
-          pixel_count++;
+        for (int i = 0; i < cp_bytes; i ++){
+          fb[(j+y)*W+x+i] = pixels[i];
         }
+        pixels+= w;
       }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
