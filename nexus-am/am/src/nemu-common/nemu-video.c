905de8fc20312a335269981861b4f8eab9510197
tracer-ics2019 <tracer@njuics.org>
2019-11-02 15:32:07 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     15:32:07 up  6:58,  1 user,  load average: 0.36, 0.27, 0.21
    63e9360375a43e09de350c15c147ccf2e275504

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -8,7 +8,7 @@
 static inline int min(int x, int y) {
   return (x < y) ? x : y;
 }
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+//static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 //static uint32_t fb[W * H] = {};
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
@@ -27,6 +27,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       int pixel_count = 0;
       uint32_t *pixels = ctl->pixels;
