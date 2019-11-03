5facb8d87b18b6bbaa5cde703df7c1577c1e6e9f
181220075-Zhou Qile <qile0402@163.com>
2019-11-03 23:26:25 +0800

    PA2 FINAL VERSION, with a little bug in coremark

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -9,7 +9,7 @@
 #define Log(format, ...) \
     _Log("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
-/*assert()的升级版，当测试条件为假时，在assertion fail之前可以输出一些信息*/
+/*assert()的升级版，当测试条件为假时， 在assertion fail之前可以输出一些信息*/
 #define Assert(cond, ...) \
   do { \
     if (!(cond)) { \
