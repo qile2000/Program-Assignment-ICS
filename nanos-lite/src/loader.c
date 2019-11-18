35ce68eb6b13fc84a58dcf5d1b7dcf60d5c91cc6
tracer-ics2019 <tracer@njuics.org>
2019-11-18 19:40:40 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     19:40:40 up  2:18,  1 user,  load average: 0.83, 0.30, 0.12
    f0c86701f6b2d54155db50c4954751716e5a863

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,10 +13,38 @@
 
 extern size_t ramdisk_read(void* buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
+extern size_t ramdisk_write(const void* buf, size_t offset, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern size_t  fs_read(int fd, void *buf, size_t len);
+extern size_t fs_filesz(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  
   ramdisk_read((void*)DEFAULT_ENTRY,0,get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
+  /*
+ int fd =fs_open(filename,0,0);
+  size_t len = fs_filesz(fd);
+  uint32_t my_ab=0,my_pages;
+  my_ab = len&0xfff;
+  my_pages = len>>12;
+  void *my_phy,*my_vir;
+  for(uint32_t i=0; i<my_pages; i++){
+	  my_phy = new_page(1);
+	  my_vir = (void *)(uintptr_t)(DEFAULT_ENTRY+(i<<12));
+	  _map(&pcb->as,my_vir,my_phy,1);
+	  fs_read(fd,my_phy,1<<12);
+  }
+  if(my_ab){
+          my_phy = new_page(1);
+	  my_vir = (void *)(uintptr_t)(DEFAULT_ENTRY+(my_pages<<12));
+	  _map(&pcb->as,my_vir,my_phy,1);
+	  fs_read(fd,my_phy,my_ab);
+  }
+  pcb -> max_brk = DEFAULT_ENTRY + len;
+  pcb -> cur_brk = DEFAULT_ENTRY + len;
+  return DEFAULT_ENTRY;
+  */
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
