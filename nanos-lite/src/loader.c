e490aed3af1c9367bf8f65c40e5a58fa73bea8fc
tracer-ics2019 <tracer@njuics.org>
2019-11-20 18:53:20 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     18:53:20 up  1:02,  1 user,  load average: 0.87, 0.63, 0.43
    13d19fc7700c8fc62bf8f6296a36ed2497df62

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,14 +10,17 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-
+//extern void isa_paddr_write(uint32_t addr, uint32_t src, int len);
 extern size_t ramdisk_read(void* buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 extern size_t ramdisk_write(const void* buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  
-  ramdisk_read((void*)0x3000000,0,get_ramdisk_size());
+  //uint32_t clear = 0;
+  ramdisk_read((void*)0x00100000,0x001000,0x09ae0);
+  //isa_paddr_write((0x00100000+0x09ae0),clear,(0x00100000+0x09ae0));
+  ramdisk_read((void*)0x0010a000,0x00b000,0x29828);
+  //isa_paddr_write((0x0010a000+0x00000),clear,(0x0010a000+0x29828));
   return (uintptr_t)0x3000000;
 }
 
