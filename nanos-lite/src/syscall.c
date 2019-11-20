1063bb5a17dae0c62d50cd577e257de8b36e39b0
181220075-Zhou Qile <qile0402@163.com>
2019-11-21 02:08:22 +0800

    sys_yield

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,10 +4,17 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0]) {
+    case SYS_yield: c->GPRx=sys_yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
+}
+int sys_yield(){
+	_yield();
+	return 0;
 }
