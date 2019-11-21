2088155f2a8b8d040b311b334982d7df6c74ab4e
tracer-ics2019 <tracer@njuics.org>
2019-11-21 11:54:11 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     11:54:11 up  1:33,  1 user,  load average: 0.28, 0.24, 0.19
    c228f53bc2892ddf49cadafdd0b60ec6c972fef

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,7 +9,7 @@ _Context* do_syscall(_Context *c) {
   a[3] = c->GPR4;
   switch (a[0]) {
     case SYS_yield: _yield(); c->GPRx=0; break;
-    default: printf("123: %x",a[0]);panic("Unhandled syscall ID = %d", a[0]);
+    default: printf("123: %s",a[0]);panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
