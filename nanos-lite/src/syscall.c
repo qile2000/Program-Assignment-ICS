adc3271cfe44cbb3b3b58ac8b51bf46d2bbebc94
181220075-Zhou Qile <qile0402@163.com>
2019-11-21 19:00:00 +0800

    sys_write

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,8 +10,19 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_yield: _yield(); c->GPRx=0; break;
     case SYS_exit: _halt(a[1]); break;
+    case SYS_write: c->GPRx=sys_write((int)a[1],(const void*)a[2],(size_t)a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
+}
+
+int sys_write(int fd,const void *buf,size_t len){
+  if(fd==1||fd==2){
+    char* _buf=(char*)buf;
+		for(int i=0;i<len;i++){
+			_putc(_buf[i]);
+    }
+	}
+	return len;
 }
