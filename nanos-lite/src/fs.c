9069967485901bc88b6626f18ff69ce54da4881b
tracer-ics2019 <tracer@njuics.org>
2019-11-27 18:34:47 +0800

    >  compile
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     18:34:47 up  2:01,  1 user,  load average: 0.48, 0.33, 0.21
    ac5dffe75faf9a06622cddd95c8141d522a81402

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,6 +3,8 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
+extern size_t ramdisk_read(void* buf, size_t offset, size_t len);
+
 typedef struct {
   char *name;
   size_t size;
