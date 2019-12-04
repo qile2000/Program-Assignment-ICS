f16385809105733c269ac79178ed0a7453b24cb0
181220075-Zhou Qile <qile0402@163.com>
2019-12-04 19:55:10 +0800

    serial write v2.0

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -66,8 +66,9 @@ size_t fs_read(int fd, void *buf, size_t len){
   //printf("len:%d\n",len);
   return len;
 }
+
 size_t fs_write(int fd, const void *buf, size_t len){
-	/*
+  /*
   if(fd==FD_STDOUT || fd==FD_STDERR){
 	char* _buf=(char*)buf;
 	for(int i=0;i<len;i++){
@@ -80,13 +81,14 @@ size_t fs_write(int fd, const void *buf, size_t len){
 	file_table[fd].write(buf, 0, len);
   }
   size_t flsz=get_file_size(fd);
-	if(flsz-file_table[fd].open_offset<len){
-		len = flsz - file_table[fd].open_offset;
+  if(flsz-file_table[fd].open_offset<len){
+	len = flsz - file_table[fd].open_offset;
   }
   ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
   file_table[fd].open_offset=file_table[fd].open_offset+len;
   return len;
 }
+
 size_t fs_lseek(int fd,size_t offset,int whence){
 	switch(whence){
 		case SEEK_SET: 
@@ -109,6 +111,7 @@ size_t fs_lseek(int fd,size_t offset,int whence){
 			return 0;
  	}
 }
+
 int fs_close(int fd){
   return 0;
 }
