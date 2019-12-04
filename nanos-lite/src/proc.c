77c51c548808bb4c418a855621ca40c0721722b7
tracer-ics2019 <tracer@njuics.org>
2019-12-04 20:36:20 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     20:36:20 up  6:03,  1 user,  load average: 0.72, 0.48, 0.34
    f2762e024786f354770dc8fb6fda923155f6cf95

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-  naive_uload(NULL, "/bin/event");
+  naive_uload(NULL, "/dev/event");
   Log("Initializing processes...");
 
   // load program here
