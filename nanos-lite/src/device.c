d565ad1cb7ed4def05aa99fe533c341bc026d4b3
181220075-Zhou Qile <qile0402@163.com>
2019-12-05 18:42:46 +0800

    dispinfo_read v1.0

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,7 +46,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  memcpy(buf,dispinfo+offset,len);
+  return strlen(buf);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
@@ -58,13 +59,15 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+  draw_sync();
+  assert(0);
   return 0;
 }
 
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-
+  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",screen_width(),screen_height());
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
