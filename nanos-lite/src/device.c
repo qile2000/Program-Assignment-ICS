6e60ba0d0ee11b2b840dab4d9cc376c4da3348cd
tracer-ics2019 <tracer@njuics.org>
2019-12-05 14:31:27 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     14:31:27 up 11:02,  1 user,  load average: 0.33, 0.19, 0.20
    d251deb27500efe345567be2f12231a3539c1dac

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include <amdev.h>
 
+
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   char* _buf=(char*)buf;
 	for(int i=0;i<len;i++){
@@ -19,6 +20,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  /*
   int key = read_key();
   if(key!=_KEY_NONE){
     bool down = false;
@@ -37,6 +39,21 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	  snprintf(buf,len,"the time is: %d\n",the_time);
   }
   return strlen(buf);
+  */
+  int key = read_key();
+  bool down = false;
+  if(key & 0x8000) {
+	  key ^= 0x8000;
+	  down = true;
+  }
+  uint32_t time=uptime();
+  if(key != _KEY_NONE) {
+	 return sprintf(buf, "%s %s\n" , down ? "kd" : "ku", keyname[key]);
+  }
+  else{
+	 return  sprintf(buf, "t %d\n", time);
+  } 
+  assert(0);
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
