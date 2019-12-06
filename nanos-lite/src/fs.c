eaaa811860c6ee55336bfdd7f64637a401636d11
tracer-ics2019 <tracer@njuics.org>
2019-12-06 13:27:37 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     13:27:37 up  4:16,  1 user,  load average: 0.55, 0.73, 0.61
    f01a1169402c0997c21ab13470e4122f81005deb

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -78,6 +78,7 @@ size_t fs_read(int fd, void *buf, size_t len){
   //printf("len:%d\n",len);
   return len;
 */
+/*
 	switch(fd){
 		case FD_EVENTS: case FD_DISPINFO:{
 			//printf("EVENTS_READ\n");
@@ -100,6 +101,19 @@ size_t fs_read(int fd, void *buf, size_t len){
   			
   			return len;
 		}
+	}
+	*/
+if(file_table[fd].read==NULL){
+	size_t fz=get_file_size(fd);
+	if(fz-file_table[fd].open_offset<len)
+		len = fz - file_table[fd].open_offset;
+	ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
+	file_table[fd].open_offset += len;
+	return len;}
+	else{
+		size_t l=file_table[fd].read(buf, file_table[fd].open_offset, len);
+		file_table[fd].open_offset+=len;
+		return l;
 	}
 }
 
