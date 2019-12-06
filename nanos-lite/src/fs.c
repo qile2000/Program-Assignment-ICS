45a32520f4dfd0ab3d427e413ade41ffefdd7ba0
tracer-ics2019 <tracer@njuics.org>
2019-12-06 13:31:47 +0800

    >  run
    qile
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     13:31:47 up  4:21,  1 user,  load average: 0.53, 0.60, 0.59
    2061db6c9904bcd2cc01c4c271f2a38f9c758960

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -103,13 +103,15 @@ size_t fs_read(int fd, void *buf, size_t len){
 		}
 	}
 	*/
-if(file_table[fd].read==NULL){
-	size_t fz=get_file_size(fd);
-	if(fz-file_table[fd].open_offset<len)
-		len = fz - file_table[fd].open_offset;
-	ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
-	file_table[fd].open_offset += len;
-	return len;}
+	printf("%s\n",file_table[fd].name);
+	if(file_table[fd].read==NULL){
+		size_t fz=get_file_size(fd);
+		if(fz-file_table[fd].open_offset<len)
+			len = fz - file_table[fd].open_offset;
+		ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
+		file_table[fd].open_offset += len;
+		return len;
+	}
 	else{
 		size_t l=file_table[fd].read(buf, file_table[fd].open_offset, len);
 		file_table[fd].open_offset+=len;
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-  naive_uload(NULL, "/bin/events");
+  naive_uload(NULL, "/bin/pal");
   Log("Initializing processes...");
 
   // load program here
